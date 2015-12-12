693b33ca6b4d294ec2b475204f86a928f11cfd1d
juanchoVanegas <sebastian44van@gmail.com>
2015-12-11 20:54:12 -0500

    Commit de introducccion a ADO

---
+++ IntroduccionADO1/IntroduccionADO/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace IntroduccionADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(13, 13);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(95, 13);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesconectar.TabIndex = 1;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(177, 13);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(136, 23);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "Consultar Categorías";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(479, 207);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ IntroduccionADO1/IntroduccionADO/Form1.cs
@@ -0,0 +1,82 @@
+﻿using IntroduccionADO.VO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroduccionADO
+{
+    public partial class Form1 : Form
+    {
+
+        private string cnnStr = "Data source=127.0.0.1; Initial Catalog=Northwind;Integrated Security=true;";
+        private SqlConnection cnn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            cnn = new SqlConnection(cnnStr);
+            cnn.Open();
+            if (cnn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Conectado a la BD Northwind");
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            ValidarConexion();
+            string sql = "SELECT * FROM Categories";
+            DataTable tbl = new DataTable();
+            SqlDataAdapter adaptador = new SqlDataAdapter(sql, cnn);
+            adaptador.Fill(tbl);
+            dataGridView1.DataSource = tbl;
+
+
+            /*
+            List<CategoryVO> categorias = new List<CategoryVO>();
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) {
+                categorias.Add(ObtenerCategoria(reader));
+            }
+            cnn.Close();
+            dataGridView1.DataSource = categorias;
+             */ 
+        }
+
+        private static CategoryVO ObtenerCategoria(SqlDataReader reader)
+        {
+            CategoryVO vo = new CategoryVO()
+            {
+                CategoryID = reader.GetInt32(0),
+                CategoryName = reader.GetString(1),
+                Description = reader.GetString(2)
+            };
+            return vo;
+        }
+
+        private void ValidarConexion()
+        {
+            if (cnn == null)
+            {
+                cnn = new SqlConnection(cnnStr);
+            }
+
+            if (cnn.State != ConnectionState.Open)
+            {
+                cnn.Open();
+            }
+        }
+    }
+}
