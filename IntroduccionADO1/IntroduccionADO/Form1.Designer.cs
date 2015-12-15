889d8078d67827cabf80504dec5db1064f2d4fcc
juanchoVanegas <sebastian44van@gmail.com>
2015-12-14 20:48:54 -0500

    Se agregan opciones de actualizar

--- IntroduccionADO1/IntroduccionADO/Form1.Designer.cs
+++ IntroduccionADO1/IntroduccionADO/Form1.Designer.cs
@@ -31,13 +31,28 @@
             this.btnConectar = new System.Windows.Forms.Button();
             this.btnDesconectar = new System.Windows.Forms.Button();
             this.btnConsultar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lstCategorias = new System.Windows.Forms.ListBox();
+            this.txtNombreActualizar = new System.Windows.Forms.TextBox();
+            this.txtDescripcionActualizar = new System.Windows.Forms.TextBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(13, 13);
+            this.btnConectar.Location = new System.Drawing.Point(6, 6);
             this.btnConectar.Name = "btnConectar";
             this.btnConectar.Size = new System.Drawing.Size(75, 23);
             this.btnConectar.TabIndex = 0;
@@ -47,7 +62,7 @@
             // 
             // btnDesconectar
             // 
-            this.btnDesconectar.Location = new System.Drawing.Point(95, 13);
+            this.btnDesconectar.Location = new System.Drawing.Point(88, 6);
             this.btnDesconectar.Name = "btnDesconectar";
             this.btnDesconectar.Size = new System.Drawing.Size(75, 23);
             this.btnDesconectar.TabIndex = 1;
@@ -56,7 +71,7 @@
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(177, 13);
+            this.btnConsultar.Location = new System.Drawing.Point(170, 6);
             this.btnConsultar.Name = "btnConsultar";
             this.btnConsultar.Size = new System.Drawing.Size(136, 23);
             this.btnConsultar.TabIndex = 2;
@@ -64,25 +79,143 @@
             this.btnConsultar.UseVisualStyleBackColor = true;
             this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(647, 352);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.btnConectar);
+            this.tabPage1.Controls.Add(this.btnDesconectar);
+            this.tabPage1.Controls.Add(this.btnConsultar);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(811, 326);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Consultar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnGuardar);
+            this.tabPage2.Controls.Add(this.txtDescripcion);
+            this.tabPage2.Controls.Add(this.txtNombre);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(811, 326);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Insertar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btnActualizar);
+            this.tabPage3.Controls.Add(this.txtDescripcionActualizar);
+            this.tabPage3.Controls.Add(this.txtNombreActualizar);
+            this.tabPage3.Controls.Add(this.lstCategorias);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(639, 326);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Actualizar";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView1.Location = new System.Drawing.Point(6, 35);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(479, 207);
-            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Size = new System.Drawing.Size(799, 285);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(113, 29);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(126, 23);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Agregar Categoría";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(6, 33);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtDescripcion.TabIndex = 8;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(6, 6);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // lstCategorias
+            // 
+            this.lstCategorias.FormattingEnabled = true;
+            this.lstCategorias.Location = new System.Drawing.Point(7, 7);
+            this.lstCategorias.Name = "lstCategorias";
+            this.lstCategorias.Size = new System.Drawing.Size(185, 303);
+            this.lstCategorias.TabIndex = 0;
+            this.lstCategorias.SelectedIndexChanged += new System.EventHandler(this.lstCategorias_SelectedIndexChanged);
+            // 
+            // txtNombreActualizar
+            // 
+            this.txtNombreActualizar.Location = new System.Drawing.Point(199, 7);
+            this.txtNombreActualizar.Name = "txtNombreActualizar";
+            this.txtNombreActualizar.Size = new System.Drawing.Size(420, 20);
+            this.txtNombreActualizar.TabIndex = 1;
+            // 
+            // txtDescripcionActualizar
+            // 
+            this.txtDescripcionActualizar.Location = new System.Drawing.Point(199, 34);
+            this.txtDescripcionActualizar.Multiline = true;
+            this.txtDescripcionActualizar.Name = "txtDescripcionActualizar";
+            this.txtDescripcionActualizar.Size = new System.Drawing.Size(420, 129);
+            this.txtDescripcionActualizar.TabIndex = 2;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(199, 170);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 3;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 262);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnConsultar);
-            this.Controls.Add(this.btnDesconectar);
-            this.Controls.Add(this.btnConectar);
+            this.ClientSize = new System.Drawing.Size(672, 376);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -93,7 +226,18 @@
         private System.Windows.Forms.Button btnConectar;
         private System.Windows.Forms.Button btnDesconectar;
         private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtDescripcionActualizar;
+        private System.Windows.Forms.TextBox txtNombreActualizar;
+        private System.Windows.Forms.ListBox lstCategorias;
     }
 }
 
--- IntroduccionADO1/IntroduccionADO/Form1.cs
+++ IntroduccionADO1/IntroduccionADO/Form1.cs
@@ -15,12 +15,13 @@ namespace IntroduccionADO
     public partial class Form1 : Form
     {
 
-        private string cnnStr = "Data source=127.0.0.1; Initial Catalog=Northwind;Integrated Security=true;";
+        private string cnnStr = "Data source=192.168.7.79;Initial Catalog=Northwind;Integrated Security=false;User Id=IntecapNET;Password=IntecapNET;";
         private SqlConnection cnn;
 
         public Form1()
         {
             InitializeComponent();
+            cargarListaCategorias();
         }
 
         private void btnConectar_Click(object sender, EventArgs e)
@@ -78,5 +79,85 @@ namespace IntroduccionADO
                 cnn.Open();
             }
         }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string descripcion = txtDescripcion.Text;
+            string sql =
+                string.Format("INSERT INTO Categories(CategoryName, Description) " +
+                              "VALUES('{0}', '{1}')", nombre, descripcion);
+
+            ValidarConexion();
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            int result = cmd.ExecuteNonQuery();
+            if (result>0)
+            {
+                MessageBox.Show("Categoria Guardada");
+            }
+        }
+
+
+        private void cargarListaCategorias() {
+            ValidarConexion();
+            string sql = "SELECT CategoryID, CategoryName FROM Categories";
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            DataTable td = new DataTable();
+            new SqlDataAdapter(sql, cnn).Fill(td);
+
+            lstCategorias.DisplayMember = "CategoryName";
+            lstCategorias.ValueMember = "CategoryID";
+            lstCategorias.DataSource = td;
+
+        }
+
+        private void lstCategorias_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstCategorias.SelectedValue!=null)
+            {
+                int id = Convert.ToInt32(lstCategorias.SelectedValue);
+                ValidarConexion();
+                string sql =
+                    string.Format("SELECT CategoryName, Description FROM Categories WHERE CategoryID={0}", id);
+                SqlCommand cmd = new SqlCommand(sql, cnn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                CategoryVO vo = new CategoryVO();
+                if (reader.Read())
+                {
+                    vo.CategoryName = reader.GetString(0);
+                    vo.Description = reader.GetValue(1).ToString();
+                    txtNombreActualizar.Text = vo.CategoryName;
+                    txtDescripcionActualizar.Text = vo.Description;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró la categoría " + lstCategorias.Text);
+                }
+                cnn.Close();    
+            }
+            
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombreActualizar.Text;
+            string descripcion = txtDescripcionActualizar.Text;
+            int id = Convert.ToInt32(lstCategorias.SelectedValue);
+            ValidarConexion();
+            string sql = string.Format(
+                "UPDATE Categories SET CategoryName='{0}', Description='{1}' WHERE CategoryID={2}",
+                nombre, descripcion, id);
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            if (cmd.ExecuteNonQuery()>0)
+            {
+                MessageBox.Show("Registro actualizado");
+                cargarListaCategorias();
+            }
+        }
+
+
+
+
     }
 }
